a378752c27f7035cdc9f9e9d55d848b5ed404dd9
igrrrind <107808162+igrrrind@users.noreply.github.com>
2025-07-05 19:09:54 +0700

    fix

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { getUsers, getUserById, updateUser, deleteUser } = require('../controllers/user.controller');
-
-
+const { getUsers, getUserById, updateUser, deleteUser, createUser } = require('../controllers/user.controller');
 
 router.get('/', getUsers);
 router.post('/', createUser);
