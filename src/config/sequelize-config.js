e05ecdc7ea195fea6ceb27f9d0ebc6955e40c31c
igrrrind <107808162+igrrrind@users.noreply.github.com>
2025-07-21 16:52:56 +0700

    add mysql2 dependcy

--- src/config/sequelize-config.js
+++ src/config/sequelize-config.js
@@ -1,4 +1,6 @@
 require('dotenv').config(); // loads from .env
+const mysql2 = require('mysql2');
+
 
 module.exports = {
   development: {
@@ -7,6 +9,7 @@ module.exports = {
     database: process.env.DB_NAME || 'database_development',
     host: process.env.DB_HOST || '127.0.0.1',
     dialect: 'mysql',
+    dialectModule: mysql2,
     dialectOptions: {
       ssl: {
         require: true,
@@ -20,6 +23,7 @@ module.exports = {
     database: process.env.DB_NAME_TEST || 'database_test',
     host: process.env.DB_HOST || '127.0.0.1',
     dialect: 'mysql',
+    dialectModule: mysql2,
   },
   production: {
     username: process.env.DB_USER || 'root',
@@ -27,6 +31,7 @@ module.exports = {
     database: process.env.DB_NAME_PROD || 'database_production',
     host: process.env.DB_HOST || '127.0.0.1',
     dialect: 'mysql',
+    dialectModule: mysql2,
     dialectOptions: {
       ssl: {
         require: true,
